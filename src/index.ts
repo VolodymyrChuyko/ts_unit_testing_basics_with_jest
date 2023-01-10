c5c3260e11194a7a1c6e408d9fe4513191cf824b
Volodymyr Chuiko <volodymyr.chuiko@sigma.software>
2023-01-10 13:46:53 +0200

    Implement functions to be tested

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import { sayHi } from "./components";
+export function sum(a: number, b: number) {
+  return a + b;
+}
 
-console.log('Hello, world!');
+export function subtract(a: number, b: number) {
+  return a - b;
+}
 
-sayHi('World');
+export function fetchUser() {
+  return new Promise<{id: number, name: string}>(resolve => {
+    setTimeout(
+    () => resolve({ id: 1, name: 'Jhon Doe' }),
+    1000);
+  });
+}
